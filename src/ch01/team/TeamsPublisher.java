d926700fa29a4ad683ed5ca4fe03f95e9b0b191f
wutalk <wu_yaowen@163.com>
2014-08-08 18:00:48 +0800

    change localhost to IP and tested on linux

--- src/ch01/team/TeamsPublisher.java
+++ src/ch01/team/TeamsPublisher.java
@@ -1,10 +1,22 @@
 package ch01.team;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.xml.ws.Endpoint;
+
 class TeamsPublisher {
-    public static void main(String[ ] args) {
-       int port = 9877;
-       String url = "http://localhost:" + port + "/teams";
-       System.out.println("Publishing Teams on port " + port);
-       Endpoint.publish(url, new Teams());
-    }
-} 
+	public static void main(String[] args) {
+		String host = "localhost";
+		try {
+			host = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			System.out.println("Error: getHostAddress failed");
+		}
+		int port = 9877;
+		String url = "http://" + host + ":" + port + "/teams";
+		System.out.println("Publishing Teams on port " + port);
+		System.out.println("listening on url: " + url);
+		Endpoint.publish(url, new Teams());
+	}
+}
